6d4c1d80ef78b1c223ca1dba4c795d81cc15cf67
TakeroxD <takeroxd@gmail.com>
2019-11-30 19:21:01 -0600

    gg

---
+++ js/logout.js
@@ -0,0 +1,9 @@
+
+$('#yesLogoutButton').on('click',function(){
+	//proceso logout
+	location.href='./index.html';
+})
+
+$('#noLogoutButton').on('click',function(){
+	location.href='./index.html';
+})
---
+++ js/signup.js
@@ -0,0 +1,5 @@
+
+$('#signupButton').on('click',function(){
+	//proceso signup
+	location.href='./index.html';
+})
