d30dceee0c0d7c223f9a5dafca106bc572615ede
TakeroxD <takeroxd@gmail.com>
2019-12-01 19:35:59 -0600

    gg

--- js/index.js
+++ js/index.js
@@ -10,3 +10,11 @@ $('#loginButton').on('click',function(){
 $('#logoutButton').on('click',function(){
 	location.href='./logout.html';
 })
+
+$('.input_wrap').on('click', function(event){
+  $(this).addClass('input_wrap_sel');
+});
+
+$('.input_wrap').focusout(function(event){
+  $(this).removeClass('input_wrap_sel');
+});
--- js/login.js
+++ js/login.js
@@ -1,7 +1,30 @@
 
 $('#loginButton').on('click',function(){
 	//proceso login
-	
-  	
-	location.href='./index.html';
+	let email = $('#email').val()
+  	let password = $('#password').val()
+
+	json_to_send = {
+		"email": email,
+		"password" : password
+	};
+
+  	json_to_send = JSON.stringify(json_to_send)
+  	console.log(json_to_send)
+  	$.ajax({
+    	url: 'https://gamelistwebapp.herokuapp.com/login',
+    	headers: {
+        	'Content-Type':'application/json'
+    	},
+    	method: 'POST',
+    	dataType: 'json',
+    	data: json_to_send,
+    	success: function(data){
+      		localStorage.setItem('token', data.token)
+      		window.location = './index.html'
+    	},
+    	error: function(error_msg) {
+      		alert((error_msg["responseText"]))
+    	}
+  	})
 })
