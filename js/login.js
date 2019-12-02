f9e1affa897df63f4ba0cbf1188d777f5b6f0251
TakeroxD <takeroxd@gmail.com>
2019-12-02 04:39:05 -0600

    gg

--- js/login.js
+++ js/login.js
@@ -1,36 +1,37 @@
 
 $('#loginButton').on('click',function(){
 	//proceso login
-	let email = $('#email').val()
+	let username = $('#user').val()
   let password = $('#password').val()
 
 	json_to_send = {
-		"email": email,
+		"username": username,
 		"password" : password
 	};
 
-  	json_to_send = JSON.stringify(json_to_send)
-  	$('body').addClass('waiting')
-  	$.ajax({
-    	url: 'https://gamelistwebapp.herokuapp.com/login',
-    	headers: {
-        	'Content-Type':'application/json'
-    	},
-    	method: 'POST',
-    	dataType: 'json',
-    	data: json_to_send,
-    	success: function(data){
-      		localStorage.setItem('token', data.token)
-          localStorage.setItem('user',data.username)
-          localStorage.setItem('userId',data._id)
-      		$('body').removeClass('waiting')
-      		window.location = './index.html'
-    	},
-    	error: function(error_msg) {
-    		$('body').removeClass('waiting')
-      		alert((error_msg["responseText"]))
-    	}
-  	})
+  json_to_send = JSON.stringify(json_to_send)
+  console.log(json_to_send)
+  $('body').addClass('waiting')
+  $.ajax({
+   	url: 'https://gamelistwebapp.herokuapp.com/login',
+   	headers: {
+       	'Content-Type':'application/json'
+   	},
+   	method: 'POST',
+   	dataType: 'json',
+   	data: json_to_send,
+   	success: function(data){
+     		localStorage.setItem('token', data.token)
+         localStorage.setItem('user',data.username)
+         localStorage.setItem('userId',data._id)
+     		$('body').removeClass('waiting')
+     		window.location = './index.html'
+   	},
+   	error: function(error_msg) {
+   		$('body').removeClass('waiting')
+     		alert((error_msg["responseText"]))
+   	}
+  })
 })
 
 $('.input_wrap').on('click', function(event){
